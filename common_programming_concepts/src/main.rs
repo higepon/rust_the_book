d6be1166ed2f157bcdf577e32071881209f8f31a
Taro Minowa <higepon@gmail.com>
2022-07-24 15:24:49 +0900

    variables

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 5;
+    println!("x={x}");
+
+    let mut y = 6;
+    println!("y={y}");
+    y = 7;
+    println!("y={y}");
+
+    const SEVEN: u32 = 7;
+    println!("seven={SEVEN}");
+
+    let z = 5;
+    let z = z + 2;
+    println!("z = {z}");
+    {
+        let z = z * 2;
+        println!("z={z}");
+    }
+    println!("z={z}");
 }
