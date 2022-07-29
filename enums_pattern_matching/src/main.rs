9d8d09dbafd7667916d7e19a2aff7992408a1cb3
Taro Minowa <higepon@gmail.com>
2022-07-30 08:49:05 +0900

    IpAddr

--- enums_pattern_matching/src/main.rs
+++ enums_pattern_matching/src/main.rs
@@ -8,10 +8,25 @@ struct IpAddr {
     address: String,
 }
 
+enum IpAddr2 {
+    V4(String),
+    V6(String),
+}
+
+enum IpAddr3 {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
 fn main() {
     let four = IpAddrKind::V4;
     let home = IpAddr {
         kind: IpAddrKind::V4,
         address: String::from("127.0.0.1"),
     };
+
+    let home = IpAddr2::V4(String::from("127.0.0.1"));
+
+    let home = IpAddr3::V4(127, 0, 0, 1);
+    let loopback = IpAddr3::V6(String::from(":1"));
 }
