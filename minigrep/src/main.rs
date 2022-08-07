1149b32dddba4dc9bf7cc174968adde04a815d12
Taro Minowa <higepon@gmail.com>
2022-08-07 10:03:39 +0900

    tests

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,21 +1,60 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 struct Config {
     query: String,
     filename: String,
 }
 
-fn parse_args(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Config { query, filename }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename).expect("file do not found");
+    println!("contetns={contents}");
+    Ok(())
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_args(&args);
-
-    let contents = fs::read_to_string(config.filename).expect("file do not found");
-    println!("contetns={contents}");
+    if args.len() < 3 {
+        panic!("what's up?");
+    }
+    let config = Config::new(&args);
+    if let Err(e) = run(config) {
+        println!("Application error {}", e);
+        process::exit(1);
+    }
 }
