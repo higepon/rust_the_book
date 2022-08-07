ce8de3ff58c373b4b6e09c23231b0841d20607fc
Taro Minowa <higepon@gmail.com>
2022-08-07 09:51:27 +0900

    config rect

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,21 @@
 use std::env;
 use std::fs;
 
-fn parse_args(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-    (query, filename)
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_args(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+    Config { query, filename }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_args(&args);
+    let config = parse_args(&args);
 
-    let contents = fs::read_to_string(filename).expect("file do not found");
+    let contents = fs::read_to_string(config.filename).expect("file do not found");
     println!("contetns={contents}");
 }
