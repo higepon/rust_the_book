21fbec9fa6cb609f1a320d9e6b5703dace3de085
Taro Minowa <higepon@gmail.com>
2022-08-07 09:48:48 +0900

    show file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
+fn parse_args(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+    (query, filename)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let (query, filename) = parse_args(&args);
+
+    let contents = fs::read_to_string(filename).expect("file do not found");
+    println!("contetns={contents}");
 }
