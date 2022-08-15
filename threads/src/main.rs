c945b61860cd4f3934595ae2689cf1c4b8b3b098
Taro Minowa <higepon@gmail.com>
2022-08-15 15:00:47 +0900

    threads + move

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -8,9 +8,17 @@ fn main() {
             thread::sleep(Duration::from_millis(1));
         }
     });
+    handle.join().unwrap();
     for i in 1..5 {
         println!("hi number {i} in main");
         thread::sleep(Duration::from_millis(1));
     }
-    handle.join().unwrap();
+
+    {
+        let v = vec![1, 2, 3];
+        let handle = thread::spawn(move || {
+            println!("vector {:?}", v);
+        });
+        handle.join().unwrap();
+    }
 }
